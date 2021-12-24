d90473c3fec77619fb9f223e1fca340e057c27a6
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2021-12-24 16:24:30 -0300

    Include frontend data and assets

---
+++ src/data/frontend.js
@@ -0,0 +1,57 @@
+export const FrontendTools = [
+    {
+        name: 'HTML5',
+        url: 'https://html.spec.whatwg.org/dev/',
+        logo: 'frontend/html5.svg',
+    },
+    {
+        name: 'Javascript - Mozilla',
+        url: 'https://developer.mozilla.org/en-US/docs/Web/javascript',
+        logo: 'frontend/javascript.png',
+    },
+    {
+        name: 'VueJS',
+        url: 'https://v3.vuejs.org/',
+        logo: 'frontend/vue.png',
+    },
+    {
+        name: 'ReactJS',
+        url: 'https://reactjs.org/docs/getting-started.html',
+        logo: 'frontend/reactjs.svg',
+    },
+    {
+        name: 'React Native',
+        url: 'https://reactnative.dev/',
+        logo: 'frontend/reactjs.svg',
+    },
+    {
+        name: 'Angular 2',
+        url: 'https://angular.io/start',
+        logo: 'frontend/angular.svg',
+    },
+    {
+        name: 'Blade',
+        url: 'https://laravel.com/docs/8.x/blade',
+        logo: 'backend/laravel.svg',
+    },
+    {
+        name: 'Livewire',
+        url: 'https://laravel-livewire.com/docs',
+        logo: 'frontend/livewire.svg',
+    },
+    {
+        name: 'Inertia',
+        url: 'https://inertiajs.com/',
+        logo: 'frontend/php_inertia.svg',
+    },
+    {
+        name: 'Bootstrap',
+        url: 'https://getbootstrap.com/docs/5.1/getting-started/introduction/',
+        logo: 'frontend/bootstrap.svg',
+    },
+    {
+        name: 'Tailwind CSS',
+        url: 'https://tailwindcss.com/docs/',
+        logo: 'frontend/tailwind.svg',
+    }
+]
