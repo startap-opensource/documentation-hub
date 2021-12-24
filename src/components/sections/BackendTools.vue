a3203aa7ddbf65cf0e349efac5e0d64f576ac829
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2021-12-24 12:21:39 -0300

    Fix on backend tools section

--- src/components/sections/BackendTools.vue
+++ src/components/sections/BackendTools.vue
@@ -3,24 +3,24 @@
     <h1 class="text-3xl font-bold my-5 text-accent-content">Backend Tools</h1>
 
     <div class="grid grid-flow-row grid-cols-4 gap-7">
-      <div v-for="(tool, index) in backendTools.php" :key="index"
-           class="card bg-base-200">
-        <figure class="justify-center pt-10 pb-5 px-10">
-          <img :src="getToolImage(tool.logo)" :alt="tool.name" class="max-h-20">
-        </figure>
-        <div class="card-body justify-between">
-          <h3 class="card-title">{{ tool.name }}</h3>
+      <div v-for="(tool, index) in backendTools" :key="index" class="card bg-base-200">
+        <div class="card-body justify-center pb-5">
 
-          <div class="card-actions">
-            <a class="btn btn-secondary btn-outline btn-sm text-xs" :href="tool.url" target="_blank">
-              Visit documentation
-
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                   class="inline-block w-6 h-6 ml-2 stroke-current">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
-              </svg>
-            </a>
+          <div class="justify-center">
+            <img :src="getToolImage(tool.logo)" :alt="tool.name"
+                 class="min-h-16 max-h-20 mx-auto">
           </div>
+
+          <div class="tool__information">
+<!--            <h3 class="card-title text-center">{{ tool.name }}</h3>-->
+
+          </div>
+        </div>
+
+        <div class="card-footer">
+          <a class="btn btn-block btn-wide btn-ghost rounded-none" :href="tool.url" target="_blank">
+            Visit documentation
+          </a>
         </div>
       </div>
     </div>
@@ -28,24 +28,24 @@
 </template>
 
 <script>
-import { phpTools, pythonTools } from "@/data/backend";
+import {phpTools, pythonTools} from "@/data/backend";
 
 export default {
   name: "BackendTools",
 
   data() {
     return {
-      backendTools: {
-        php: phpTools,
-        python: pythonTools,
-      }
+      backendTools: [
+        ...phpTools,
+        ...pythonTools,
+      ],
     };
   },
 
   methods: {
     getToolImage(tool) {
       return tool === '' ? '' : require(`../../assets/logos/${tool}`);
-    }
+    },
   }
 }
 </script>
