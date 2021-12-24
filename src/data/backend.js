f609938f96e1c7a2e32c479ad1d483cba47ecdd3
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2021-12-23 22:14:43 -0300

    #1 - Include new backend tools

---
+++ src/data/backend.js
@@ -0,0 +1,37 @@
+const languages = {
+    php: 'PHP',
+    python: 'Python',
+    javascript: 'Javascript',
+    java: 'Java',
+}
+
+/**
+ * PHP related tools
+ */
+export const phpTools = [
+    {name: 'PHP Language Documentation', language: languages.php, url: 'https://www.php.net/docs'},
+    {name: 'Laravel', language: languages.php, url: 'https://laravel.com/docs'},
+    {name: 'CodeIgniter', language: languages.php, url: 'https://codeigniter.com/user_guide/index.html'},
+    {name: 'Symfony', language: languages.php, url: 'https://symfony.com/doc/current/index.html'},
+    {name: 'CakePHP', language: languages.php, url: 'https://book.cakephp.org/4/en/index.html'},
+]
+
+export const pythonTools = [
+    /**
+     * Python related tools
+     */
+    {name: 'Python Language Documentation', language: languages.python, url: 'https://www.python.org/doc/'},
+    {name: 'Django Project', language: languages.python, url: 'https://docs.djangoproject.com/'},
+    {name: 'Flask', language: languages.python, url: 'https://flask.palletsprojects.com/en/2.0.x/'},
+    {name: 'FastAPI', language: languages.python, url: 'https://fastapi.tiangolo.com/'},
+]
+
+/**
+ * Java Related tools
+ */
+// const javaTools = []
+
+/**
+ * JavaScript Related tools
+ */
+// const javascriptTools = []
