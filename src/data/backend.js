69814af7fdfb9cb5c0fd6272104108773361827e
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2021-12-24 15:32:51 -0300

    Format Backend JS data

--- src/data/backend.js
+++ src/data/backend.js
@@ -2,21 +2,57 @@
  * PHP related tools
  */
 export const phpTools = [
-    {name: 'PHP Docs', url: 'https://www.php.net/docs', logo: 'backend/php.svg'},
-    {name: 'Laravel', url: 'https://laravel.com/docs', logo: 'backend/laravel.svg'},
-    {name: 'CodeIgniter', url: 'https://codeigniter.com/user_guide/index.html', logo: 'backend/codeigniter.svg'},
-    {name: 'Symfony', url: 'https://symfony.com/doc/current/index.html', logo: 'backend/symfony.svg'},
-    {name: 'CakePHP', url: 'https://book.cakephp.org/4/en/index.html', logo: 'backend/cake-logo.svg'},
+    {
+        name: 'PHP Docs',
+        url: 'https://www.php.net/docs',
+        logo: 'backend/php.svg'
+    },
+    {
+        name: 'Laravel',
+        url: 'https://laravel.com/docs',
+        logo: 'backend/laravel.svg'
+    },
+    {
+        name: 'CodeIgniter',
+        url: 'https://codeigniter.com/user_guide/index.html',
+        logo: 'backend/codeigniter.svg'
+    },
+    {
+        name: 'Symfony',
+        url: 'https://symfony.com/doc/current/index.html',
+        logo: 'backend/symfony.svg'
+    },
+    {
+        name: 'CakePHP',
+        url: 'https://book.cakephp.org/4/en/index.html',
+        logo: 'backend/cake-logo.svg'
+    },
 ]
 
+/**
+ * Python related tools
+ */
 export const pythonTools = [
-    /**
-     * Python related tools
-     */
-    {name: 'Python Docs', url: 'https:/www.python.org/doc/', logo: 'backend/python.svg'},
-    {name: 'Django Project', url: 'https://docs.djangoproject.com/', logo: 'backend/django.svg'},
-    {name: 'Flask', url: 'https://flask.palletsprojects.com/en/2.0.x/', logo: 'backend/flask-logo.png'},
-    {name: 'FastAPI', url: 'https://fastapi.tiangolo.com/', logo: 'backend/fastapi.png'},
+    {
+        name: 'Python Docs',
+        url: 'https:/www.python.org/doc/',
+        logo: 'backend/python.svg'
+    },
+    {
+        name: 'Django Project',
+        url: 'https://docs.djangoproject.com/',
+        logo: 'backend/django.svg'
+    },
+    {
+        name: 'Flask',
+        url: 'https://flask.palletsprojects.com/en/2.0.x/',
+        logo: 'backend/flask-logo.png'
+    },
+    {
+        name: 'FastAPI',
+        url: 'https://fastapi.tiangolo.com/',
+        logo: 'backend/fastapi.png'
+    },
 ]
 
 /**
