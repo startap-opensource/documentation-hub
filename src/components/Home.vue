49026dd7345dfc30f19e13a36ed0453fcf1ded42
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2021-12-23 22:13:39 -0300

    Include hero for HomePage

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <homepage-hero :hero-svg="heroSvg"/>
+
+  <div class="my-2">
+    <backend-tools></backend-tools>
+  </div>
+</template>
+
+<script>
+import heroSvg from '@/assets/illustrations/programming.svg'
+
+import BackendTools from '@/components/sections/BackendTools'
+import HomepageHero from "@/components/sections/HomepageHero";
+
+export default {
+  name: "Homepage",
+  components: {HomepageHero, BackendTools},
+  setup() {
+    return {
+      heroSvg
+    }
+  },
+}
+</script>
--- src/components/sections/HomepageHero.vue
+++ src/components/sections/HomepageHero.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="hero min-h-screen bg-base-200">
     <div class="flex-col hero-content lg:flex-row-reverse">
-      <img src="https://picsum.photos/id/1005/600/600" class="max-w-sm rounded-lg shadow-2xl">
+      <img :src="heroSvg" class="max-w-lg">
       <div>
         <h1 class="mb-5 text-5xl font-bold">
           A single place to find your documentation.
         </h1>
         <p class="mb-5">
-          Where you can find References and Documentations of every tool used on your project.
-          Backend, Frontend, Libraries, Frameworks, anything (or you can make a Pull Request with more tools)
+          Find and search any documentation used on your project.
+          <br>
+          <i>(Or submit a pull request to update this amazing tool that we built together!)</i>
         </p>
         <button class="btn btn-primary">Search a tool</button>
       </div>
     </div>
   </div>
 </template>
-
 <script>
 export default {
-  name: "Homepage"
+  name: 'HomepageHero',
+  props: {
+    heroSvg: {}
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
