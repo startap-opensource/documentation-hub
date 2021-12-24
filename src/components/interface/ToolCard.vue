147e9bca04fe8652abc0f1a9f0322c7e303c7374
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2021-12-24 15:30:37 -0300

    Create new ToolCard component

---
+++ src/components/interface/ToolCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card bg-base-200">
+    <div class="card-body justify-center pb-5">
+
+      <div class="justify-center">
+        <img :src="getToolImage(currentTool.logo)" :alt="currentTool.name"
+             class="min-h-16 max-h-20 mx-auto">
+      </div>
+
+      <div class="tool__information">
+      </div>
+    </div>
+
+    <div class="card-footer">
+      <a class="btn btn-block btn-wide btn-outline btn-info border-0 rounded-none" :href="currentTool.url" target="_blank">
+        Visit documentation
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToolCard.vue",
+
+  props: {
+    currentTool: {
+      type: Object,
+      required: true,
+    }
+  },
+
+  methods: {
+    getToolImage(logoImage) {
+      return require(`/src/assets/logos/${logoImage}`);
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/sections/BackendTools.vue
+++ src/components/sections/BackendTools.vue
@@ -1,37 +1,25 @@
 <template>
-  <div class="row px-10 py-10 bg-base-content">
+  <div class="row px-10 py-10 row px-10 py-10 bg-gradient-to-br from-indigo-800 to-indigo-900">
     <h1 class="text-3xl font-bold my-5 text-accent-content">Backend Tools</h1>
 
     <div class="grid grid-flow-row grid-cols-4 gap-7">
-      <div v-for="(tool, index) in backendTools" :key="index" class="card bg-base-200">
-        <div class="card-body justify-center pb-5">
-
-          <div class="justify-center">
-            <img :src="getToolImage(tool.logo)" :alt="tool.name"
-                 class="min-h-16 max-h-20 mx-auto">
-          </div>
-
-          <div class="tool__information">
-<!--            <h3 class="card-title text-center">{{ tool.name }}</h3>-->
-
-          </div>
-        </div>
-
-        <div class="card-footer">
-          <a class="btn btn-block btn-wide btn-ghost rounded-none" :href="tool.url" target="_blank">
-            Visit documentation
-          </a>
-        </div>
-      </div>
+      <tool-card v-for="(tool, index) in backendTools"
+                 :key="index"
+                 :current-tool="tool">
+      </tool-card>
     </div>
   </div>
 </template>
 
 <script>
 import {phpTools, pythonTools} from "@/data/backend";
+import ToolCard from "@/components/interface/ToolCard";
 
 export default {
   name: "BackendTools",
+  components: {
+    ToolCard
+  },
 
   data() {
     return {
@@ -41,12 +29,6 @@ export default {
       ],
     };
   },
-
-  methods: {
-    getToolImage(tool) {
-      return tool === '' ? '' : require(`../../assets/logos/${tool}`);
-    },
-  }
 }
 </script>
 
